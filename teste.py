c9222ae185372f10bea90fa55e1c0d3a8a40e578
siconeli <henrique.siconeli.f@hotmail.com>
2024-10-16 19:18:11 -0400

    up

--- teste.py
+++ teste.py
@@ -1,48 +1,53 @@
-# import matplotlib.pyplot as plt
-# import tempfile
-# import os
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg') 
 
-# categorias = ['A', 'B', 'C', 'D', 'E']
-# valores = [10, 23, 17, 12, 15]
+class RelatorioGrafico(LoginRequiredMixin, View):
+    def get(self, request):
+        pdf = FPDF()
+        pdf.add_page()
 
-# plt.bar(categorias, valores)
-# plt.xlabel('Categorias')
-# plt.ylabel('Valores')
-# plt.title('Gráfico de Barras')
-# # plt.show()
-# plt.savefig('grafico.png')
-# plt.close()
+        pdf.image('static/img/layout.png', 0, 0, 210, 297)
 
-# criar o arquivo temporário, em seguida excluílo
+        
+        categorias = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+        valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 0]
+        
+        largura_colunas = 0.8
 
-# Pegar o layout do canva e preencher a página do pdf com o layout
+        plt.figure(figsize=(20,10))
 
-# 
+        plt.bar(categorias, valores, width=largura_colunas)
+        
+        plt.title('Gráfico de arrecadação de tributos')
+        
+        plt.ylabel('Valores')
+        
+        plt.xlabel('Meses')
+        
+        plt.savefig('static/img/grafico.png')
+        
+        plt.close()
 
-# from fpdf import FPDF
-# import tempfile
-# import os
-# from django.http import HttpResponse
+        pdf.image('static/img/grafico.png', -10, 40, 230) #x=30, y=40, tamanho imagem=200
 
-# def GerarPdf():
-#     pdf = FPDF()
+        pdf.set_font("Arial", size=12)  # Defina a fonte e o tamanho
 
-#     pdf.add_page()
+        # Adicione uma célula com o texto
+        pdf.cell(200, 10, txt="Relatório de arrecadação de tributos", ln=True, align='C')
 
-#     pdf.cell("teste")
+        # Crie um arquivo temporário
+        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.pdf')
+        pdf.output(temp_file.name)
 
-#     temp_file = tempfile.NamedTemporaryFile(delete=False)
-#     pdf.output(temp_file.name)
-#     temp_file.close()
+        temp_file.close()
+        
+        with open(temp_file.name, 'rb') as file:
+            pdf_content = file.read()
+        
+        os.unlink(temp_file.name)  # Exclua o arquivo temporário
 
-#     with open(temp_file.name, 'rb') as file:
-#         pdf_content = file.read()
+        response = HttpResponse(pdf_content, content_type='application/pdf')
+        response['Content-Disposition'] = 'inline; filename="grafico.pdf"'
 
-#     os.unlink(temp_file.name)
-
-#     response = HttpResponse(pdf_content, content_type='application/pdf')
-#     response['Content-Disposition'] = 'inline; filename="teste.pdf"'
-
-#     return response
-
-# GerarPdf()
+        return response
