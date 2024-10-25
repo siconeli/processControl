aaee1ccfd19e9b812cdf6a02b16afeac9bec6150
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2024-10-25 16:38:22 -0400

    up

--- apps/grafico/views.py
+++ apps/grafico/views.py
@@ -346,6 +346,7 @@ class GerarRelatorioGrafico(LoginRequiredMixin, View):
                     meses = meses_list[indice_filtro_1:indice_filtro_2 +1]
 
                     valores_2 = list(objeto[mes] for mes in meses )  
+                    print(valores_2[0])
         else:
             valores_2 = valores
 
@@ -363,23 +364,26 @@ class GerarRelatorioGrafico(LoginRequiredMixin, View):
         # plt.plot(meses_1, valores_1, marker='o', linestyle='-', color='b', label=ano_1)
         # plt.plot(meses_2, valores_2, marker='o', linestyle='--', color='r', label=ano_2)
 
-        # Adicionar valores em cima das barras
+        # Função para formatar o valor em R$
+        def formatar_valor(valor):
+            return f'{valor:,.2f}'.replace(',', 'X').replace('.', ',').replace('X', '.')
+
+        # Adicionar valores em cima das barras com formatação R$
         for bar in bars1:
             yval = bar.get_height()  # Obtém a altura da barra
-            plt.text(bar.get_x() + bar.get_width() / 2, yval, int(yval), ha='center', va='bottom')  # Adiciona o texto
+            plt.text(bar.get_x() + bar.get_width() / 2, yval, formatar_valor(yval), ha='center', va='bottom')  # Adiciona o texto com rotação
 
-        # Adicionar valores em cima das barras
         for bar in bars2:
             yval = bar.get_height()  # Obtém a altura da barra
-            plt.text(bar.get_x() + bar.get_width() / 2, yval, int(yval), ha='center', va='bottom')  # Adiciona o texto
+            plt.text(bar.get_x() + bar.get_width() / 2, yval, formatar_valor(yval), ha='center', va='bottom')  # Adiciona o texto com rotação
 
         # Alterar a cor de fundo da área do gráfico
         plt.gca().set_facecolor('#eeeeee') 
 
         # Título e rótulos
         # plt.title('Gráfico de Arrecadação de Tributos')
-        plt.ylabel('Valores')
-        plt.xlabel('Meses')
+        plt.ylabel('Valores', fontsize=14)
+        plt.xlabel('Meses', fontsize=14)
         plt.xticks(x, meses, fontsize=15)  # Rótulos dos meses
         plt.yticks(fontsize=14)  # Rótulos da média de valores
         plt.legend()  # Exibir legenda
@@ -391,12 +395,12 @@ class GerarRelatorioGrafico(LoginRequiredMixin, View):
         # Imagem do gráfico
         pdf.image('static/img/grafico.png', -15, 25, 240) #x=-15, y=22, tamanho imagem=240
 
-        pdf.set_font("Arial", size=15)  # Defina a fonte e o tamanho
+        pdf.set_font("Arial", style='B', size=15)  # Defina a fonte e o tamanho
         pdf.set_text_color(255, 255, 255)  # RGB para branco
         pdf.cell(200, 2, txt="ARRECADAÇÃO MENSAL DE TRIBUTOS", ln=True, align='C')
 
         # Text Receita
-        pdf.set_font("Arial", size=15)  # Defina a fonte e o tamanho
+        pdf.set_font("Arial", style='B', size=15)  # Defina a fonte e o tamanho
         pdf.set_text_color(255, 255, 255)  # RGB para branco
         pdf.cell(200, 12, txt=receita.nome, ln=True, align='C')
 
